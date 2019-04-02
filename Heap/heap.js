553a1db34a7de2f4c1711cd3ef94da6a108206c3
Ducnm <duc2820@gmail.com>
2019-04-02 16:04:57 +0700

    update data struct heap and custom aglo

--- Heap/heap.js
+++ Heap/heap.js
@@ -1,128 +1,76 @@
-/**
- * A binary heap is a complete binary tree which
- * satisfies the heap ordering property.
- *
- * var heap = new Heap(function(a, b) {
- *     return a.birthyear - b.birthyear;
- * });
- *
- * heap.add({
- *     name: 'John',
- *     birthyear: 1981
- * });
- * heap.add({
- *     name: 'Pavlo',
- *     birthyear: 2000
- * });
- * heap.add({
- *     name: 'Garry',
- *     birthyear: 1989
- * });
- * heap.add({
- *     name: 'Derek',
- *     birthyear: 1990
- * });
- * heap.add({
- *     name: 'Ivan',
- *     birthyear: 1966
- * });
- *
- * console.log(heap.extract()); // { name: 'Pavlo', birthyear: 2000 }
- * console.log(heap.extract()); // { name: 'Derek', birthyear: 1990 }
- * console.log(heap.extract()); // { name: 'Garry', birthyear: 1989 }
- * console.log(heap.extract()); // { name: 'John', birthyear: 1981 }
- * console.log(heap.extract()); // { name: 'Ivan', birthyear: 1966 }
- */
-(function (exports) {
-
-  'use strict';
-  exports.Heap = function (cmp) {
-    this._heap = [];
-    if (typeof cmp === 'function') {
-      this._cmp = cmp;
-    } else {
-      // default sortx`
-      this._cmp = function (a, b) {
-        return a - b;
-      };
+class Heap {
+  heap = []
+  constructor(cmp = undefined) {
+    this.heap = []
+    if (cmp && typeof cmp !== 'function') {
+      this.cmp = cmp
     }
-  };
-
-
-  exports.Heap.prototype._heapify = function (index) {
-    var extr = index;
-    var left = 2 * index + 1;
-    var right = 2 * index + 2;
-    var temp;
-
-    if (left < this._heap.length && this._cmp(this._heap[left], this._heap[index]) > 0) {
-      extr = left;
+    else {
+      this.cmp = (a, b) => { return a - b }
     }
+  }
+  log() {
+    console.log(this.heap)
+  }
+  changeKey = (index, value) => {
 
-    if (right < this._heap.length &&
-        this._cmp(this._heap[right], this._heap[index]) > 0 &&
-        this._cmp(this._heap[right], this._heap[left]) > 0) {
-      extr = right;
-    }
-
-    if (index !== extr) {
-      temp = this._heap[index];
-      this._heap[index] = this._heap[extr];
-      this._heap[extr] = temp;
-      this._heapify(extr);
-    }
-  };
-
-
-  exports.Heap.prototype.changeKey = function (index, value) {
-    this._heap[index] = value;
-    var elem = this._heap[index];
-    var parent = Math.floor(index / 2);
-    var temp;
-    if (elem !== undefined) {
-      while (parent >= 0 && this._cmp(elem, this._heap[parent]) > 0) {
-        temp = this._heap[parent];
-        this._heap[parent] = elem;
-        this._heap[index] = temp;
-        index = parent;
-        parent = Math.floor(parent / 2);
+    let temp;
+    let parentIndex = Math.floor(index / 2)
+    if (this.heap[index]) {
+      // refactor branch tree 
+      while (parentIndex >= 0 && this.cmp(this.heap[index], this.heap[parentIndex]) > 0) {
+        temp = this.heap[index]
+        this.heap[index] = this.heap[parentIndex]
+        this.heap[parentIndex] = temp
+        index = parentIndex
+        parentIndex = Math.floor(parentIndex / 2)
       }
-      console.log('find parent key' , index)
-      this._heapify(index);
+      this.heapify(index)
+
     }
-    return parent;
-  };
-
-  exports.Heap.prototype.update = function (node) {
-    var idx = this._heap.indexOf(node);
-    if (idx >= 0) {
-      this.changeKey(idx, node);
+  }
+  heapify = (index) => {
+    //  ok branch swap elements
+    let temp
+    let left = 2 * index + 1
+    let right = 2 * index + 2
+    let exct = index
+    if (left < this.heap.length && this.cmp(this.heapify[left], this.heapify[index])) {
+      left = exct
     }
-  };
-
-  exports.Heap.prototype.add = function (value) {
-    this._heap.push(value);
-    return this.changeKey(this._heap.length - 1, value);
-  };
-
-  exports.Heap.prototype.top = function () {
-    return this._heap[0];
-  };
-  exports.Heap.prototype.extract = function () {
-    if (!this._heap.length) {
-      throw 'The heap is already empty!';
+    if (right < this.heap.length && this.cmp(this.heapify[right], this.heapify[index])
+      && this.cmp(this.heapify[right], this.heapify[left])
+    ) {
+      right = exct;
     }
-    var extr = this._heap.shift();
-    this._heapify(0);
-    return extr;
-  };
+    if(exct !== index){
+      temp = this.heap[exct];
+       this.heap[exct]  = this.heap[index]
+       this.heap[index] = temp
+      this.heapify(exct)
+    }
+  }
+  add(value) {
+    this.heap.push(value)
+    this.changeKey(this.heap.length - 1, value)
+  }
+  extract() {
 
-  exports.Heap.prototype.getCollection = function () {
-    return this._heap;
-  };
+  }
 
-  exports.Heap.prototype.isEmpty = function () {
-    return !this._heap.length;
-  };
+}
+var heap = new Heap()
+heap.add(2)
 
-})(typeof window === 'undefined' ? module.exports : window);
+heap.add(1)
+heap.add(4)
+
+heap.add(3)
+heap.add(7)
+heap.add(9)
+heap.add(6)
+heap.add(19)
+heap.add(91)
+
+
+heap.log()
---
+++ LinkList/linklist.js
@@ -0,0 +1,16 @@
+/*
+    linar struct consist link list and  array
+*/
+class Node  {
+    value 
+    constructor(value) {
+        this.value = value
+    }
+}
+class LinkList {
+    Node node ;
+    constructor(){
+
+    }
+   
+};
