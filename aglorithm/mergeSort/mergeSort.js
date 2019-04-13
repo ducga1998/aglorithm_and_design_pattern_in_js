239de1a8b7fe0d6fe3b4ec95ecdd70bfcbc97bd9
Ducnm <duc2820@gmail.com>
2019-04-13 15:57:38 +0700

    update linklist, graph and quicksort

--- aglorithm/mergeSort/mergeSort.js
+++ aglorithm/mergeSort/mergeSort.js
@@ -7,19 +7,20 @@ function MergeSort(arr = [] ,name = ''){
     if(arr.length < 2) {
         return arr.sort()
     }
-    return mergeTopDown( MergeSort(left,'left'),MergeSort(right, 'right'))
+    return mergeTopDown(MergeSort(left,'left'),MergeSort(right, 'right'))
 }
 function mergeTopDown(left, right) {
     const array =[]
   
-    while(left.length && right.length) {
-      if(left[0] < right[0]) {
-        array.push(left.shift());
-      } else {
-        array.push(right.shift());
-      }
-    }
-    
+   while(left.length && right.length) {
+     if(left[0] < right[0]){
+       array.push(left.shift())
+     }
+     else {
+       array.push(right.shift())
+     }
+   }
+    console.log('array ==== >',array , right)
     return array.concat(left.concat(right));
   }
   
